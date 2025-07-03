d54365cab955ccf25ec7f730e1fc93a499e52075
Maria Aparecida <maria.cavalcante.2008@aluno.edu.pi.gov.br>
2025-07-03 11:12:34 -0300

    Atualizacao

--- aula01-classe-objeto/src/aula01/MainConta.java
+++ aula01-classe-objeto/src/aula01/MainConta.java
@@ -1,11 +1,13 @@
 package aula01;
 
-public class MainConta {
+public c
+
+    ass MainConta {
     public static void main(String[] args) {
         Conta c1 = new Conta("Maria Betania", 2234 , 50.00);
 
         var resposta = c1.depositar(100.00);
-        
+
         var saque = c1.sacar(30.00);
         System.out.println(resposta);
         System.out.println(saque);
